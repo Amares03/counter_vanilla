c995560ac5d99f5dd950be7ac5f5e3d69b3a973f
Amares03 <amaressabit0314@gmail.com>
2022-05-18 15:01:11 +0300

    set condition

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("reset")) {
+      count = 0;
+    } else {
+      count++;
+    }
+  });
+});
